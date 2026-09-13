ff4f08d5c7483f33a11269dcab11bcc009b819af
Srivatsa <vatsa@crimp.local>
2026-09-13 22:05:09 +0100

    nvim-agent-comments and make oil watch for file changes

--- lua/vatsa/lazy/nvim-agent-comments.lua
+++ lua/vatsa/lazy/nvim-agent-comments.lua
@@ -1,8 +1,26 @@
 return {
-    dir = '/Users/vatsa/Documents/Coding/FunProjects/nvim-agent-comments',
-    name = 'nvim-agent-comments',
+    "WorldOccupier/nvim-agent-comments",
     lazy = false,
-    config = function()
-        require('nvim-agent-comments').setup()
-    end,
+    opts = {
+        signs = true,
+        navigation = true,
+        store_name = ".nvim-agent-comments.json",
+        context_lines = 2,
+        keymaps = {
+            n = {
+                ["<leader>aca"] = "<cmd>NvimAgentCommentsAdd<cr>",
+                ["<leader>ace"] = "<cmd>NvimAgentCommentsEdit<cr>",
+                ["<leader>acd"] = "<cmd>NvimAgentCommentsDelete<cr>",
+                ["<leader>acj"] = "<cmd>NvimAgentCommentsJump<cr>",
+                ["<leader>acl"] = "<cmd>NvimAgentCommentsList<cr>",
+                ["<leader>acr"] = "<cmd>NvimAgentCommentsReanchor<cr>",
+                ["<leader>acs"] = "<cmd>NvimAgentCommentsSearch<cr>",
+                ["<leader>act"] = "<cmd>NvimAgentCommentsRetrieve<cr>",
+            },
+            x = {
+                ["<leader>aca"] = ":NvimAgentCommentsAddVisual<cr>",
+                ["<leader>acr"] = ":NvimAgentCommentsReanchor<cr>",
+            },
+        },
+    },
 }
